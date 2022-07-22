8711c6703f983e3edac92ebc23db8506802bf7b9
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-07-22 05:53:32 +0300

    Support outputFlag

--- cmd/mstrusted/mstrusted.go
+++ cmd/mstrusted/mstrusted.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/jedisct1/go-minisign"
 	"oskarsh.ru/mstrusted"
@@ -27,14 +30,19 @@ func main() {
 
 	pubKey, comment, err := mstrusted.SearchTrustedPubKey(sigFile)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("Error: %v\n", err)
 	}
-	log.Printf("Verifying with %v (%v)\n", comment, mstrusted.EncodeID(pubKey.KeyId))
+	log.Printf("Verifying with %v (%v).\n", comment, mstrusted.EncodeID(pubKey.KeyId))
 	_, err = verify(pubKey, file, sigFile)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("Error: %v\n", err)
 	}
 	log.Printf("Signature and comment signature verified.")
+	if outputFlag {
+		if err := outputFile(file); err != nil {
+			log.Fatalf("Error: %v\n", err)
+		}
+	}
 }
 
 func verify(key minisign.PublicKey, file string, sigFile string) (bool, error) {
@@ -43,4 +51,25 @@ func verify(key minisign.PublicKey, file string, sigFile string) (bool, error) {
 		return false, err
 	}
 	return key.VerifyFromFile(file, s)
+}
+
+func outputFile(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	const maxChunk = 4096
+	b := make([]byte, maxChunk)
+	for {
+		readTotal, err := file.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			break
+		}
+		fmt.Printf("%s", b[:readTotal])
+	}
+	return nil
 }
